a0af6641aa4a8e9b19c7057d5bc5767e5b0a1659
Sahan Ravindu <sahanrv@outlook.com>
2024-03-04 09:24:38 +0530

    Initial Commit

---
+++ SRCryptographicSecurityEncription/SRCryptographicSecurityEncriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRCryptographicSecurityEncriptionApp.swift
+//  SRCryptographicSecurityEncription
+//
+//  Created by Sahan Ravindu on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SRCryptographicSecurityEncriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
